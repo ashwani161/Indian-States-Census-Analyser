76b3416a65c207afe7c46028e15bf9ce4ffe7bec
ashwani161 <ashwanin65@gmail.com>
2021-04-07 16:32:06 +0530

    Added: Welcome message to Indian States Census Analyser

---
+++ src/main/java/IndianStatesCensusAnalyser/CensusAnalyser.java
@@ -0,0 +1,8 @@
+package IndianStatesCensusAnalyser;
+
+public class CensusAnalyser {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian States Census Analyser");
+	}
+	
+}
